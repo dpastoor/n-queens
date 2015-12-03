9e295bb4e32583b2efd58b9d2cf5074bf2e34ba8
Devin Pastoor <devin.pastoor@gmail.com>
2015-12-03 10:35:05 -0800

    control slider style

--- src/App.js
+++ src/App.js
@@ -7,17 +7,21 @@ export class App extends Component {
         super(props);
         this.state = {n: 5}
     }
+
     updateSlider(e, value) {
        this.setState({
             n: value
         });
     }
   render() {
+      let boardSize = 600;
     return (
        <div>
-           <Slider name="mySlider" ref="mySlider" defaultValue={5} step={1} min={1} max={8} onChange={this.updateSlider.bind(this)} />
+           <div style={{height: '50px', width:boardSize }}>
+               <Slider name="mySlider" ref="mySlider" defaultValue={5} step={1} min={1} max={8} onChange={this.updateSlider.bind(this)} />
+           </div>
 
-           <Board n={this.state.n} gridSize={600} />
+           <Board n={this.state.n} gridSize={boardSize} />
        </div>
     );
   }
