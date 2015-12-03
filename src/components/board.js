a37083f161ee4c6e92dd4e139404e989c06426f3
Devin Pastoor <devin.pastoor@gmail.com>
2015-12-02 18:57:37 -0800

    styling and control setup

--- src/components/board.js
+++ src/components/board.js
@@ -6,18 +6,35 @@ import React, { Component } from 'react';
 import mui from 'material-ui';
 import {GridList, GridTile, IconButton} from 'material-ui';
 export default class Board extends React.Component {
+
 render() {
-    let tilesData = [{title: 'test1', author:'devin'}, {title: 'test2', author:'albert'}];
+    let n = 3
+    let gridSize = 600;
+    let cellTotalSize = gridSize-20;
+    let tilesData = [
+        {title: 'test1', author:'devin'},
+        {title: 'test3', author:'devin'},
+        {title: 'test4', author:'devin'},
+        {title: 'test5', author:'devin'},
+        {title: 'test6', author:'devin'},
+        {title: 'test2', author:'albert'},
+        {title: 'test7', author:'devin'},
+        {title: 'test8', author:'albert'},
+        {title: 'test9', author:'albert'}
+    ];
    return(
        <GridList
-           cellHeight={200}
-           style={{width: 320, height: 640, overflowY: 'auto'}}
+           cols={n}
+           cellHeight={cellTotalSize/n}
+           style={{width: gridSize, height: gridSize, overflowY: 'auto', color: 'red', borderStyle: 'solid', borderColor: 'black',
+           textAlign: 'center', verticalAlign: 'middle'}}
       >
    {
-      tilesData.map(tile => <GridTile
-      title={tile.title}
-      subtitle={<span>by <b>{tile.author}</b></span>}
-   ><h1>test </h1></GridTile>)
+      tilesData.map(tile =>
+          <GridTile style={{borderColor:'black', borderStyle: 'solid'}}><div>
+              <h1>{tile.title} </h1>
+          </div></GridTile>
+      )
    }
     </GridList>
    )
