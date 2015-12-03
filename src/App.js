fa2ec1df46bac790496010cd2290b41733b7f19c
Devin Pastoor <devin.pastoor@gmail.com>
2015-12-02 20:31:00 -0800

    add slider to control board size

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import Board from './components/board.js'
+import {Slider} from 'material-ui';
 export class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {n: 5}
+    }
+    updateSlider(e, value) {
+       this.setState({
+            n: value
+        });
+    }
   render() {
     return (
-   <Board n={6} gridSize={600} />
+       <div>
+           <Slider name="mySlider" ref="mySlider" defaultValue={5} step={1} min={1} max={8} onChange={this.updateSlider.bind(this)} />
+
+           <Board n={this.state.n} gridSize={600} />
+       </div>
     );
   }
 }
